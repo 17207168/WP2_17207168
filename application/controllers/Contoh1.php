11f4a8b93d31320451d1e5a788a2ef04589143f5
17207168 <17207168@bsi.ac.id>
2023-10-09 21:16:26 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Amriuddin
+ Saya tinggal di daerah Bandung
+ olah raga yang saya sukai adalah
+ Sepak Bola";}
+}
